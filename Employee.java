ec077e813a673e3ac27e012212c646c0ac94a949
Timur <timur.turdaliev08@gmail.com>
2025-09-19 14:07:49 -0400

    Clean version of my Original exam answers

--- Employee.java
+++ Employee.java
@@ -10,23 +10,25 @@ public class Employee{
     private boolean onLeave;
     private double hourlyWage;
     private double unpaidHours;
-
-    public boolean Employee(String fullname, int yearOfBirth, double hourlyWage){
+    
+    public Employee(String fullname, int yearOfBirth, double hourlyWage){
         fullname = fullname;
-        private string fullname;
-        }
-            getFullname = fullname
-        }
-        private string name;
-        }
-            setFullname = writeFullname
-        }
         yearOfBirth = yearOfBirth;
         hourlyWage = hourlyWage;
         onLeave = false;
         unpaidHours = 0.0;
     } 
     
+    // q11
+    // private string fullname;
+        // }
+            // getFullname = fullname
+        // }
+        // private string name;
+        // }
+            // setFullname = writeFullname
+        // }
+        
     /**
      * Returns true is an employee is above 16 years old
      */
@@ -39,31 +41,34 @@ public class Employee{
      */
     public boolean canDrive(){
         int age = calculateAge(2025);
-        }
-          if int age ≥ 16then 
-             return = can drive
-          else 
-             return = 16 - age
-        }
-
+        // q12
+        // }
+          // if int age ≥ 16then 
+             // return = can drive
+          // else 
+             // return = 16 - age
+        // }
+        return true;
     }
 
     /*
      * Returns the net pay for the outstanding unpaid hours
      */
     private double calculatePay(){
-    int salary = calculatePay();
-    Get Pay = hyWage x unpaindHours
-    Set PayAfterTaxes = 70% x pay
-    Total print = payAfterTaxes
-    
+    // q13
+    // int salary = calculatePay();
+    // Get Pay = hyWage x unpaindHours
+    // Set PayAfterTaxes = 70% x pay
+    // Total print = payAfterTaxes
+    return 0;
     }
 
     /*
      * Output the payment record and resets unpaid hours
      */
     public void paySalary(){
-        Calculate Pay = Print Fullname + Salary
-        After pay = Reset to 0
+        // q14
+        // Calculate Pay = Print Fullname + Salary
+        // After pay = Reset to 0
     }
 }
