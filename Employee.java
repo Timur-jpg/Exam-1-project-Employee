9d35f9f532af2afcb781efaf05add411b8ff02ac
Timur <timur.turdaliev08@gmail.com>
2025-09-19 13:53:38 -0400

    Those are my exam first answers

--- Employee.java
+++ Employee.java
@@ -11,13 +11,21 @@ public class Employee{
     private double hourlyWage;
     private double unpaidHours;
 
-    public Employee(String fullname, int yearOfBirth, double hourlyWage){
-        this.fullname = fullname;
-        this.yearOfBirth = yearOfBirth;
-        this.hourlyWage = hourlyWage;
+    public boolean Employee(String fullname, int yearOfBirth, double hourlyWage){
+        fullname = fullname;
+        private string fullname;
+        }
+            getFullname = fullname
+        }
+        private string name;
+        }
+            setFullname = writeFullname
+        }
+        yearOfBirth = yearOfBirth;
+        hourlyWage = hourlyWage;
         onLeave = false;
         unpaidHours = 0.0;
-    }
+    } 
     
     /**
      * Returns true is an employee is above 16 years old
@@ -31,20 +39,31 @@ public class Employee{
      */
     public boolean canDrive(){
         int age = calculateAge(2025);
-        // to be completed
+        }
+          if int age ≥ 16then 
+             return = can drive
+          else 
+             return = 16 - age
+        }
+
     }
 
     /*
      * Returns the net pay for the outstanding unpaid hours
      */
     private double calculatePay(){
-        // to be completed
+    int salary = calculatePay();
+    Get Pay = hyWage x unpaindHours
+    Set PayAfterTaxes = 70% x pay
+    Total print = payAfterTaxes
+    
     }
 
     /*
      * Output the payment record and resets unpaid hours
      */
     public void paySalary(){
-        // to be completed
+        Calculate Pay = Print Fullname + Salary
+        After pay = Reset to 0
     }
 }
